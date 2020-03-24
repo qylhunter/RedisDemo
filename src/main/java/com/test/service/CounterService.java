d0325dd13e656a78ef35b4b8d43f6bec261ed73b
robin <qylhunter@163.com>
2020-03-24 14:39:39 +0800

    add count

--- src/main/java/com/test/service/CounterService.java
+++ src/main/java/com/test/service/CounterService.java
@@ -12,11 +12,13 @@ import org.springframework.stereotype.Component;
 public class CounterService implements InitializingBean {
 
     @Autowired
-    RedisTemplate redisTemplate;
+    RedisTemplate<String, String> redisTemplate;
 
     public void count(String key, int expire) {
         Long increment = redisTemplate.opsForValue().increment(key);
         System.out.println(increment);
+        //hash 数据类型，分类 hs:hsk 如某路径hs下各不同ip（hsk）的访问次数
+        redisTemplate.opsForHash().increment("hs", "hsk", 1);
     }
 
     @Override
@@ -24,5 +26,6 @@ public class CounterService implements InitializingBean {
         for (int i = 0; i < 3; i++) {
             count("test", 40);
         }
+        System.out.println(redisTemplate.opsForHash().get("hs", "hsk"));
     }
 }
